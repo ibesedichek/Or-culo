38645c0dbbd8c2831471ee938d5f77b9b2096058
ibesedichek <i.besedichek@uniandes.edu.co>
2025-11-06 18:29:29 +0000

    Cambios

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const titulo = document.getElementById("titulo");
+const textoInicial = document.getElementById("textoInicial");
+let estadoInicial = true;
+let posicionTexto = 0;
+const textos = ["Hola","Bienvenidos", "a", "mi","página","web"];
+
+textoInicial.innerText = textos[posicionTexto];
+
+textoInicial.addEventListener("click", () => {
+    if (posicionTexto < textos.length - 1) {
+    posicionTexto = posicionTexto + 1; 
+    textoInicial.innerText = textos[posicionTexto];
+    } else {
+        posicionTexto = 0;
+        textoInicial.innerText = textos[posicionTexto];
+    }
+
+})
 
 titulo.addEventListener("click", () => {
+    if (estadoInicial === true) {
     titulo.innerText = "Bienvenidos";
+    } else {
+        titulo.innerText = "Hola, soy la Carita Lunar";
+    }
+    estadoInicial = !estadoInicial;
 })
